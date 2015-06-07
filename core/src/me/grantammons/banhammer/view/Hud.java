5d8c0433fad3d6b4b4a12a3eca02dfe97844fe01
Grant Ammons <gammons@gmail.com>
2015-06-07 08:09:03 -0400

    refactor hud a bit

--- core/src/me/grantammons/banhammer/view/Hud.java
+++ core/src/me/grantammons/banhammer/view/Hud.java
@@ -2,7 +2,6 @@ package me.grantammons.banhammer.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import me.grantammons.banhammer.core.Constants;
@@ -18,32 +17,31 @@ public class Hud {
 
     public Hud() {
         batch = new SpriteBatch();
-
-        hudCamera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
-        hudCamera.position.set(0,0,0);
-        hudCamera.setToOrtho(true);
-        hudCamera.update();
         font = new BitmapFont( Gdx.files.internal("arial-15.fnt"), true);
-        font.getRegion().getTexture().setFilter( Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        //font.getRegion().getTexture().setFilter( Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        setupCamera();
     }
 
+
     public void resize (int width, int height) {
-        hudCamera.viewportWidth = (Constants.VIEWPORT_HEIGHT
-                / (float)height) * (float)width;
-        hudCamera.update();
         hudCamera.viewportHeight = Constants.VIEWPORT_HEIGHT;
-        hudCamera.viewportWidth = (Constants.VIEWPORT_HEIGHT
-                / (float)height) * (float)width;
-        hudCamera.position.set(hudCamera.viewportWidth / 2,
-                hudCamera.viewportHeight / 2, 0);
+        hudCamera.viewportWidth = (Constants.VIEWPORT_HEIGHT / (float)height) * (float)width;
+        hudCamera.position.set(hudCamera.viewportWidth / 2, hudCamera.viewportHeight / 2, 0);
         hudCamera.update();
     }
 
     public void render() {
         batch.setProjectionMatrix(hudCamera.combined);
         batch.begin();
-        font.draw(batch, "testing shit", 0,0);
+        font.draw(batch, "testing shit", 10,0);
         batch.end();
+    }
+
+    private void setupCamera() {
+        hudCamera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+        hudCamera.position.set(0,0,0);
+        hudCamera.setToOrtho(true);
+        hudCamera.update();
     }
 
 }
